bdd7da6926b495ac87f7fc5ea906238d175d42b8
Shahrose Kasim <rosemaster3000@gmail.com>
2023-03-27 04:37:07 -0400

    implemented uvicorn Socket.io Server

--- app.py
+++ app.py
@@ -1,34 +1,56 @@
 import socketio
-from Decorators import login_required
-sio = socketio.AsyncServer()
+from decorators import login_required
 
+sio = socketio.AsyncServer(async_mode='asgi', cors_allowed_origins=["https://www.piesocket.com"])
+app = socketio.ASGIApp(sio)
 
 @sio.event
-async def connect(sid, environ, auth):
+async def connect(sid, environ):
     print('connect ', sid)
 
 @sio.event
 async def disconnect(sid):
     print('disconnect ', sid)
 
-
 @sio.event
-@login_required
-async def whoAmI(sid, data):
-    print(sid)
-    sio.emit('youAre', {'data': sid})
+async def message(sid, data):
+    session = await sio.get_session(sid)
+    print('message from ', session['username'])
+
+@sio.on('*')
+async def catchAll(event, sid, data):
+    print("[ ? ]", event, data)
+
+
+if __name__=="__main__":
+    import uvicorn
+    uvicorn.run(app, host='localhost', port=3000)
+
+
+# @sio.event
+# @login_required
+# def whoAmI(sid, data):
+#     print(sid)
+#     sio.emit('youAre', {'data': sid})
+
+
+# @sio.event
+# def roomTest(sid, data):
+#     sio.enter_room(sid, 'cool_room')
+#     sio.emit(
+#         'my event',
+#         {'dice': 2,'chip': 11},
+#         room='cool_room'
+#     )
+#     sio.leave_room(sid, 'cool_room')
 
 
 
 
 
-@sio.event
-async def roomTest(sid, data):
-    sio.enter_room(sid, 'cool_room')
-    sio.emit(
-        'my event',
-        {'dice': 2,'chip': 11},
-        room='cool_room'
-    )
-    sio.leave_room(sid, 'cool_room')
-
+# https://python-socketio.readthedocs.io/en/latest/server.html#event-callbacks
+# https://python-socketio.readthedocs.io/en/latest/server.html#user-sessions
+# @sio.event
+# async def connect(sid, environ):
+#     username = authenticate_user(environ)
+#     await sio.save_session(sid, {'username': username})
