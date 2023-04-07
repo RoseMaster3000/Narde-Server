d7d4078acb8d31bd207d494214e353411f806ceb
Shahrose Kasim <rosemaster3000@gmail.com>
2023-04-06 23:38:52 -0400

    added comment

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+# https://github.com/settings/tokens
+# git clone https://github.com/RoseMaster3000/Narde-Server.git
 python3.9 -m venv virt
 source virt/bin/activate
 pip install -r requirements.txt
