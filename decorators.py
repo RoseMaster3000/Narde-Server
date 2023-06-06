2ff431dd90cead31e77df03f7d196b44fb670c55
Shahrose Kasim <rosemaster3000@gmail.com>
2023-06-06 17:59:40 -0400

    fixed decorators.py

--- decorators.py
+++ decorators.py
@@ -4,14 +4,14 @@ from Player import Player
 def login_required(f):
     @wraps(f)
     async def wrapper(*args, **kwargs):
-        player = Player.get(args[0])
+        sid = args[0]
+        player = Player.get(sid)
         if player == None:
             return {"status":401, "reason":'authentication required'}
-        kwargs["sid"] = args[0]
-        kwargs["data"] = args[1] if (len(args)>1) else {}
         kwargs["player"] = player
-        return await f(**kwargs)
+        return await f(*args, **kwargs)
     return wrapper
+
 
 def anon_required(f):
     @wraps(f)
@@ -23,17 +23,17 @@ def anon_required(f):
         return await f(*args, **kwargs)
     return wrapper
 
+
 def data_required(*args):
     required = list(args)
     def decorator(f):
         @wraps(f)
         async def wrapper(*args, **kwargs):
-            sid = args[0]
             data = args[1]
-            fields = ", ".join(required)
             if type(data) is not dict:
                 return {"status":400, "reason":f"[{f.__name__}] event expects dictionary (via JSON)"}
             if any([(key not in data) for key in required]):
+                fields = ", ".join(required)
                 return {"status":422, "reason":f"[{f.__name__}] event expects [{fields}] fields"}
             return await f(*args, **kwargs)
         return wrapper
