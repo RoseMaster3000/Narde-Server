6ad6e5e6995ce14fe900d322c00e2593ad79b978
RoseMaster3000 <rosemaster3000@gmail.com>
2023-06-19 05:37:03 -0400

    Update nginx.sh

--- deploy/nginx.sh
+++ deploy/nginx.sh
@@ -1,6 +1,7 @@
 sudo apt update
 sudo apt install nginx
 sudo cp deploy/nginx.conf /etc/nginx/nginx.conf
+sudo systemctl daemon-reload
 systemctl start nginx
 systemctl enable nginx
 systemctl status nginx
