5f2eae239edf27bdc0bc10d566df3157d0296e4b
Shahrose Kasim <shahros3@gmail.com>
2023-09-02 01:28:54 -0400

    updated version 0.13

--- sync.sh
+++ sync.sh
@@ -2,6 +2,7 @@
 # rm -rf "Build"
 # mkdir "Build"
 # megadl --path "Build" https://mega.nz/folder/SvQzWTDQ#dnK-FyLH-0hqrz7BUvY4Sg
+
 source deploy/stop.sh
 git pull origin main
 source deploy/start.sh
