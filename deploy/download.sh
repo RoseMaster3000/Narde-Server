cd3c90fabc3998af3e9be696707f3e34d1eda9ca
Shahrose Kasim <shahros3@gmail.com>
2023-09-10 21:43:41 -0400

    added version 0.14

---
+++ deploy/download.sh
@@ -0,0 +1,3 @@
+rm -rf "../Build"
+mkdir "../Build";
+megadl --path "../Build" https://mega.nz/folder/XjQShBAT#vILWsoKhnStDeRpVcOA1Vg
--- sync.sh
+++
@@ -1,8 +0,0 @@
-# Update / sync with github + mega.nz
-# rm -rf "Build"
-# mkdir "Build"
-# megadl --path "Build" https://mega.nz/folder/SvQzWTDQ#dnK-FyLH-0hqrz7BUvY4Sg
-
-source deploy/stop.sh
-git pull origin main
-source deploy/start.sh
